6d6a7888e36ffb8f55b28e2d5ce66ae6bde4022a
mjan112a <myersjd@gmail.com>
2025-04-28 12:25:20 -0400

    Fix deployment error by adding transpilePackages for ag-grid modules in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  transpilePackages: ['ag-grid-react', 'ag-grid-community'],
+  webpack: (config, { isServer }) => {
+    // Add a rule for handling Handlebars files
+    config.module.rules.push({
+      test: /\.handlebars$|\.hbs$/,
+      loader: 'handlebars-loader'
+    });
+
+    // Handle Handlebars runtime issues
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      handlebars: 'handlebars/dist/handlebars.min.js'
+    };
+
+    return config;
+  }
+};
 
 module.exports = nextConfig
